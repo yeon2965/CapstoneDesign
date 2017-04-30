6ec3e920a36e59327560cd5140716eb3ede8de52
Diroo <tip9004@naver.com>
2017-04-30 22:00:57 +0900

    샘플사진 목록 이미지 추가

--- CapstoneDesign_Remo/app/src/main/java/com/imaginarywings/capstonedesign/remo/LandscapeActivity.java
+++ CapstoneDesign_Remo/app/src/main/java/com/imaginarywings/capstonedesign/remo/LandscapeActivity.java
@@ -1,11 +1,18 @@
 package com.imaginarywings.capstonedesign.remo;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
+
+import java.util.ArrayList;
 
 /**
  * Created by default on 2017-04-08.
@@ -28,5 +35,31 @@ public class LandscapeActivity extends Activity{
                 startActivity(cameraIntent);
             }
         });
+
+        checkCameraPermissions();
     }
+
+    //카메라 퍼미션 요청
+    private void checkCameraPermissions()
+    {
+        new TedPermission(this)
+                .setPermissionListener(permissionlistener)
+                .setDeniedMessage("If you reject permission,you can not use this service\n\nPlease turn on permissions at [Setting] > [Permission]")
+                .setPermissions(Manifest.permission.CAMERA,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.READ_EXTERNAL_STORAGE)
+                .check();
+    }
+
+    PermissionListener permissionlistener = new PermissionListener() {
+        @Override
+        public void onPermissionGranted() {
+
+        }
+
+        @Override
+        public void onPermissionDenied(ArrayList<String> deniedPermissions) {
+            Toast.makeText(LandscapeActivity.this, "권한이 허용되지 않아서 앱을 이용할 수 없습니다.", Toast.LENGTH_SHORT).show();
+        }
+    };
 }
